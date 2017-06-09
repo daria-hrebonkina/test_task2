bc53f91c01b604857ca564345fb99e7490402ca6
dashka169 <dashka169@gmail.com>
2017-06-10 00:12:19 +0300

    fix

--- task3/task3.php
+++ task3/task3.php
@@ -5,7 +5,7 @@ if (!$db) {
     die('Ошибка соединения: ' . mysql_error());
 }
 echo 'Успешно соединились';
-
+mysql_select_db('task3');
 $categories = mysql_query("SELECT * FROM  category");
 if   (mysql_num_rows($categories) > 0){
     $cats = [];
@@ -14,20 +14,25 @@ if   (mysql_num_rows($categories) > 0){
         $cats[$cat['parent_id']][$cat['id']] =  $cat;
     }
 }
-
+/**
+ * @param array $cats array with categories
+ * @param integer $parent_id parent category id
+ * @param bool $only_parent if true - full tree of categories, else - returns only categories with current parent_id
+ * @return null|string
+ */
 function getCategories($cats, $parent_id, $only_parent = false){
     if(is_array($cats) and isset($cats[$parent_id])){
         $tree = '<ul>';
         if ($only_parent==false) {
             foreach ($cats[$parent_id] as $cat){
                 $tree .= '<li>' . $cat['name'] . ' #' . $cat['id'];
-                $tree .=  build_tree($cats, $cat['id']);
+                $tree .=  getCategories($cats, $cat['id']);
                 $tree .= '</li>';
             }
         } elseif (is_numeric($only_parent)){
             $cat = $cats[$parent_id][$only_parent];
             $tree .= '<li>' . $cat['name'] . ' #' . $cat['id'];
-            $tree .=  build_tree($cats, $cat['id']);
+            $tree .=  getCategories($cats, $cat['id']);
             $tree .= '</li>';
         }
         $tree .= '</ul>';
@@ -36,6 +41,10 @@ function getCategories($cats, $parent_id, $only_parent = false){
     return $tree;
 }
 
+/**
+ * @param integer $category_id id of category which have the films we need
+ * @return string
+ */
 function getFilms($category_id)
 {
     $films = mysql_query('SELECT `film`.`title` FROM `film` 
@@ -43,10 +52,11 @@ function getFilms($category_id)
           LEFT JOIN `category` ON `category`.`id` = `film_category`.`category_id`
           WHERE `category`.`id` = ' . $category_id);
     $filmsList = '<ul>';
-    foreach ($films as $film) {
-        $filmsList .= '<li>' . $film . '</li>';
+    while ($film = mysql_fetch_assoc($films)) {
+        $filmsList .= '<li>' . $film['title'] . '</li>';
     }
     $filmsList .= '</ul>';
     return $filmsList;
 }
-mysql_close($db);
+
+
